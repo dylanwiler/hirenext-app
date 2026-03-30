7399b1b2dddbb0310bcd9445ad35c53c69b7efcb
dylanwiler <hello@mindscript.app>
2026-03-30 18:25:15 -0500

    fix lib/supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,7 +3,7 @@ import { createBrowserClient } from '@supabase/ssr'
 // Browser client (for 'use client' components)
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUTABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
