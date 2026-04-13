b3025bec2260034e9bc33567ef0f3c83fab55964
dylanwiler <hello@mindscript.app>
2026-04-13 17:02:55 -0500

    fix auth [middleware.ts]

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,7 @@
 import { NextResponse, type NextRequest } from 'next/server'
-
 export function middleware(request: NextRequest) {
   return NextResponse.next()
 }
-
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
 }
